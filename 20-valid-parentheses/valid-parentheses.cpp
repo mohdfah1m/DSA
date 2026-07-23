e2c1dd69e78b525a76f65893409a89a69d2e9a1d
Mohammed Fahim <mohdfahim2956@gmail.com>
2026-07-23 23:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (18.57%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+#include<string.h>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c=='(' or c=='{' or c=='['){
+                st.push(c);
+            }else{
+                if(st.empty())
+                    return false;
+                
+                if((c==')' && st.top()!='(') or 
+                (c=='}' && st.top()!='{') or 
+                (c==']' && st.top()!='[')){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
