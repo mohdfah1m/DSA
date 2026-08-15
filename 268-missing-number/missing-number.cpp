3bdc704929c8cda0d1d66af84213979dc02c455d
Mohammed Fahim <mohdfahim2956@gmail.com>
2026-08-15 13:57:16 +0530

    Time: 822 ms (5.03%) | Memory: 21.8 MB (39.26%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
+        int res=n;
+        for(int i=0;i<n;i++){
+            if(nums[i] !=i){
+                res=i;
+                break;
+            }
+        }
+        return res;
+    }
+};
